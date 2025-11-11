2a54170bcd02edfed9db71207f68d0092169b1b8
andikaparamata350-a11y <andikaparamata350@gmail.com>
2025-11-11 06:05:18 -0800

    Update laporan.hs

--- laporan.hs
+++ laporan.hs
@@ -1,3 +1,29 @@
+#Membuat Struktur Directori
+mkdir documents images archives logs
+ls
+touch doc{1..5}.txt
+touch img{1..5}.jpeg
+touch arc{1..5}.zip
+echo "isi" > logs1.log
+echo "isi" > logs2.log
+echo "isi" > logs3.log
+echo "isi" > logs4.log
+echo "isi" > logs5.log
+echo "isi" > arc1.zip
+ls
+
+#Scirp organisasi file
+find . -type f -name "*txt" -exec mv {} documents/ \;
+find . -type f -name "*jpeg" -exec cp {} images/ \;
+find . -type f -name "*zip" -exec mv {} archives/ \;
+find . -type f -name "*log" -exec mv {} logs/ \;
+
+#fungsi pencarian
+find . -type f -name "*doc*"
+find ~/documents -type f -size -20k -exec ls -lh {} \;
+find ~/logs -type f -exec grep -li "isi" {} \;
+
+#Generate Laporan
 echo "==============================" > report.txt
 echo "LAPORAN STATISTIK FILE SISTEM" >> report.txt
 echo "==============================" >> report.txt
